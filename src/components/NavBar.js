d6b3779ea015a694b9b8357be7107376b0bc397b
Mukherjee <sayantan.a.mukherjee@capgemini.com>
2023-08-03 19:00:04 +0530

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,84 @@
+import React, { useState } from "react";
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+
+import { NavLink,Link } from "react-router-dom";
+import "./NavBar.css";
+import { SidebarData } from './SidebarData';
+
+
+
+function NavBar() {
+  const [sidebar, setSidebar] = useState(false);
+  const [click, setClick] = useState(false);
+  const showSidebar = () => setSidebar(!sidebar);
+  const handleClick = () => setClick(!click);
+  return (
+    <>
+      <nav className="navbar">
+        <div className="nav-container">
+          <NavLink exact to="/" className="nav-logo">
+            Bluescope Monitoring
+            <i className="fas fa-code"></i>
+          </NavLink>
+
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            
+            <li className="nav-item">
+              {/* <NavLink
+                exact
+                to="/menu"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              > */}
+              <Link to='#' className="nav-links"
+              // className='menu-bars'
+                   onClick={showSidebar} >
+              
+                Menu
+                </Link>
+              {/* </NavLink> */}
+              <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+          <ul className='nav-menu-items' onClick={showSidebar}>
+            <li className='navbar-toggle'>
+              <Link to='#' className='menu-bars'>
+                <AiIcons.AiOutlineClose />
+              </Link>
+            </li>
+            {SidebarData.map((item, index) => {
+              return (
+                <li key={index} className={item.cName}>
+                  <Link to={item.path}>
+                    {item.icon}
+                    <span>{item.title}</span>
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+            </li>
+            
+            <li className="nav-item">
+              <NavLink
+                exact
+                to="/login"
+                activeClassName="active"
+                className="nav-links"
+                onClick={handleClick}
+              >
+                Login
+              </NavLink>
+            </li>
+          </ul>
+          <div className="nav-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"}></i>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default NavBar;

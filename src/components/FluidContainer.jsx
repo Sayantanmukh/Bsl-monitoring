ef823d8698f7e39b4e815ee7fa99fed33ab38a84
Anurag Banerjee <anurag.banerjee011@gmail.com>
2023-08-11 23:21:26 +0530

    added responsive charts

---
+++ src/components/FluidContainer.jsx
@@ -0,0 +1,12 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import * as React from 'react';
+import GridComponent from './GridComponent';
+
+export default function FluidContainerComponent() {
+    return (
+        <React.Fragment>
+            <CssBaseline />
+            <GridComponent />
+        </React.Fragment>
+    );
+}
---
+++ src/components/PieChartComponent.jsx
@@ -0,0 +1,29 @@
+import { pieArcClasses, PieChart } from '@mui/x-charts/PieChart';
+import * as React from 'react';
+
+const data = [
+    { id: 0, value: 10, label: 'series A' },
+    { id: 1, value: 15, label: 'series B' },
+    { id: 2, value: 20, label: 'series C' },
+];
+
+export default function PieChartComponent() {
+    return (
+        <PieChart
+            series={[
+                {
+                    data,
+                    highlightScope: { faded: 'global', highlighted: 'item' },
+                    faded: { innerRadius: 30, additionalRadius: -30 },
+                },
+            ]}
+            sx={{
+                [`& .${pieArcClasses.faded}`]: {
+                    fill: 'gray',
+                },
+            }}
+            width={window.innerWidth/1.2}
+            height={window.innerHeight/2.2}
+        />
+    );
+}
